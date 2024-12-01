6d7360a8743e5a23de0830bd7da934f2a98dac90
Mark Hoek <mbhoek@gmail.com>
2024-12-02 00:17:39 +0100

    Day 01, Part 1

---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,9 @@
+﻿// Advent of Code 2024
+// https://github.com/mbhoek/AdventOfCode
+
+Console.WriteLine("Advent of Code 2024");
+Console.WriteLine("===================");
+
+AdventOfCode2024.Day01.Solve();
+
+Console.ReadLine();
